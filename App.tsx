454e0fee035f3e32247ff88bcfdaeb9ec4260442
Natanael Lima <taelima1997@gmail.com>
2021-05-26 10:33:08 -0300

    update index project

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Dashboard } from './src/screens/Dashboard';
+import Main from './src';
 
 const App: React.FC = () => {
-  return <Dashboard />;
+  return <Main />;
 };
 
 export default App;
