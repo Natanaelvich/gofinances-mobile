098379a8ceb420730eca99f46a0b1574d90c5474
Natanael Lima <taelima1997@gmail.com>
2021-05-26 10:40:04 -0300

    add load get fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
+import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
 import theme from './global/styles/theme';
 import { Dashboard } from './screens/Dashboard';
 
 const Main: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+  if (!fontsLoaded) return <AppLoading />;
   return (
     <ThemeProvider theme={theme}>
       <Dashboard />
