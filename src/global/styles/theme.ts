9d7a82d3a35ee00e4b488e53ec84983a15c44c40
Natanael Lima <taelima1997@gmail.com>
2021-05-26 10:33:17 -0300

    add initial theme

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,17 @@
+export default {
+  colors: {
+    primary: '#5636d3',
+    secondary: '#FF872c',
+    secondary_light: 'rgba(255,135,44, 0.3)',
+
+    success: '#12a454',
+    success_light: 'rgba(16,164,84,0.5)',
+
+    attention: '#e83f5b',
+    attention_light: 'rgba(232,63, 91, 0.5)',
+    shape: '#FFFFFF',
+    title: '#363f5f',
+    text: '#969cb2',
+    background: '#f0f2f5',
+  },
+};
