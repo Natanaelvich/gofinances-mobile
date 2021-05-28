3b5811b268da318e9b84d98a77f867be33f3dc69
Natanael Lima <taelima1997@gmail.com>
2021-05-27 22:24:06 -0300

    add header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import { Container, Title } from './styles';
+import { Container, Header } from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <Text>Ola</Text>
+      </Header>
     </Container>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -6,9 +6,7 @@ export const Container = styled.View`
   align-items: center;
   background-color: ${({ theme }) => theme.colors.background};
 `;
-export const Title = styled.Text`
-  font-size: 24px;
-  font-weight: bold;
-  color: ${({ theme }) => theme.colors.title};
-  font-family: ${({ theme }) => theme.fonts.bold};
+export const Header = styled.View`
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.primary};
 `;
