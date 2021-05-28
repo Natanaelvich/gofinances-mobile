84e3fe0180d793f2c370c5688e238b3d22960aad
Natanael Lima <taelima1997@gmail.com>
2021-05-28 14:40:05 -0300

    update Register with TransactionTypeButton

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { Button } from '../../components/Button';
+import { TransactionTypeButton } from '../../components/Forms/TransactionTypeButton';
 import { Input } from '../../components/Input';
-import { Container, Fields, Form, Header, Title } from './styles';
+import {
+  Container,
+  Fields,
+  Form,
+  Header,
+  Title,
+  TransactionTypeButtonContainer,
+} from './styles';
 
 export function Register() {
   return (
@@ -14,6 +22,19 @@ export function Register() {
         <Fields>
           <Input placeholder="Nome" />
           <Input placeholder="Valor" />
+
+          <TransactionTypeButtonContainer>
+            <TransactionTypeButton
+              type="down"
+              title="Outcome"
+              icon="arrow-down-circle"
+            />
+            <TransactionTypeButton
+              type="up"
+              title="Income"
+              icon="arrow-up-circle"
+            />
+          </TransactionTypeButtonContainer>
         </Fields>
         <Button title="Enviar" />
       </Form>
--- src/screens/Register/styles.ts
+++ src/screens/Register/styles.ts
@@ -28,3 +28,7 @@ export const Form = styled.View`
   justify-content: space-between;
 `;
 export const Fields = styled.View``;
+export const TransactionTypeButtonContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
