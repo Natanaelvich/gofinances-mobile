b74a37b1b821f179d7101640bfdceb11909974d9
Natanael Lima <taelima1997@gmail.com>
2021-05-28 14:33:19 -0300

    add button component

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components/native';
+import { TouchableOpacity } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+export const Container = styled(TouchableOpacity)`
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.secondary};
+  border-radius: 5px;
+  align-items: center;
+  padding: 18px;
+`;
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(14)}px;
+  font-family: ${({ theme }) => theme.fonts.medium};
+  text-align: center;
+`;
