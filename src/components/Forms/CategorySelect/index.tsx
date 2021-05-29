1220e387fba982719b417ecddecf51244f533a86
Natanael Lima <taelima1997@gmail.com>
2021-05-29 09:13:07 -0300

    add CategorySelect

---
+++ src/components/Forms/CategorySelect/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+function CategorySelect() {
+  return (
+    <Container>
+      <Text>CategorySelect</Text>
+    </Container>
+  );
+}
+
+export default CategorySelect;
