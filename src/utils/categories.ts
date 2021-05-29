693e9b4c9eb24a78505c78ffc9a11fed788db415
Natanael Lima <taelima1997@gmail.com>
2021-05-29 09:36:54 -0300

    add categories utils

---
+++ src/utils/categories.ts
@@ -0,0 +1,8 @@
+export const categories = [
+  { key: 'purchases', name: 'Compras', icon: 'shopping-bag', color: '#5636d3' },
+  { key: 'food', name: 'Alimentacao', icon: 'coffee', color: '#ff872c' },
+  { key: 'salary', name: 'Salario', icon: 'dollar-sign', color: '#12a454' },
+  { key: 'car', name: 'Carro', icon: 'crosshair', color: '#e83f5b' },
+  { key: 'leisure', name: 'Lazer', icon: 'heart', color: '#26195c' },
+  { key: 'studies', name: 'Estudos', icon: 'book', color: '#9c001a' },
+];
