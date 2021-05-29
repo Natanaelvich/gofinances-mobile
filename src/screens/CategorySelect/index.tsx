d50ca7abc239f351dee9ee053a5eb03922ada54c
Natanael Lima <taelima1997@gmail.com>
2021-05-29 09:42:52 -0300

    update categoryselect

--- src/screens/CategorySelect/index.tsx
+++ src/screens/CategorySelect/index.tsx
@@ -1,15 +1,53 @@
 import React from 'react';
+import { FlatList } from 'react-native';
+import { Button } from '../../components/Forms/Button';
+import { categories } from '../../utils/categories';
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  Category,
+  Name,
+  Icon,
+  Separator,
+  Footer,
+} from './styles';
 
-import { Text } from 'react-native';
+interface Category {
+  key: string;
+  name: string;
+}
+interface CategorySelectProps {
+  category: Category;
+  setCategory: (category: Category) => void;
+  closeSelectCategory: () => void;
+}
 
-import { Container } from './styles';
-
-function CategorySelect({ children }: CategorySelectProps) {
+export function CategorySelect({
+  category,
+  setCategory,
+  closeSelectCategory,
+}: CategorySelectProps) {
   return (
     <Container>
-      <Text>CategorySelect</Text>
+      <Header>
+        <HeaderTitle>Categoria</HeaderTitle>
+      </Header>
+      <FlatList
+        data={categories}
+        style={{ flex: 1, width: '100%' }}
+        keyExtractor={(category: Category) => category.key}
+        ItemSeparatorComponent={() => <Separator />}
+        renderItem={({ item }) => (
+          <Category>
+            <Icon name={item.icon} />
+            <Name>{item.name}</Name>
+          </Category>
+        )}
+      />
+      <Footer>
+        <Button title="Selecionar" onPress={closeSelectCategory} />
+      </Footer>
     </Container>
   );
 }
-
-export default CategorySelect;
--- src/screens/CategorySelect/styles.ts
+++ src/screens/CategorySelect/styles.ts
@@ -1,5 +1,46 @@
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
+import { Feather } from '@expo/vector-icons';
 
 export const Container = styled.View`
-
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background};
+`;
+export const Header = styled.View`
+  justify-content: flex-end;
+  align-items: center;
+  width: 100%;
+  height: ${RFValue(113)}px;
+  background-color: ${({ theme }) => theme.colors.primary};
+  padding-bottom: 19px;
+`;
+export const HeaderTitle = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(18)}px;
+  color: ${({ theme }) => theme.colors.shape};
+`;
+export const Category = styled.View`
+  width: 100%;
+  padding: ${RFValue(15)}px;
+  flex-direction: row;
+  align-items: center;
+`;
+export const Icon = styled(Feather)`
+  font-size: ${RFValue(20)}px;
+  margin-right: 16px;
+`;
+export const Name = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(14)}px;
+  color: ${({ theme }) => theme.colors.title};
+`;
+export const Separator = styled.View`
+  width: 100%;
+  height: 1px;
+  background-color: ${({ theme }) => theme.colors.text};
+`;
+export const Footer = styled.View`
+  width: 100%;
+  padding: 24px;
 `;
