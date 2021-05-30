13f67df5bfdf7b6a813a739ee3910234bf2a9877
Natanael Lima <taelima1997@gmail.com>
2021-05-30 10:14:35 -0300

    fix type button on modal

--- src/components/Forms/Button/styles.ts
+++ src/components/Forms/Button/styles.ts
@@ -1,8 +1,7 @@
 import styled from 'styled-components/native';
 import { RFValue } from 'react-native-responsive-fontsize';
-import { RectButton } from 'react-native-gesture-handler';
 
-export const Container = styled(RectButton)`
+export const Container = styled.TouchableOpacity`
   width: 100%;
   background-color: ${({ theme }) => theme.colors.secondary};
   border-radius: 5px;
