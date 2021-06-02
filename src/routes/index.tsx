bf4713c34ba715774814da581b420f38b005153c
Natanael Lima <taelima1997@gmail.com>
2021-06-01 23:03:55 -0300

    update routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,17 @@
-import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { AppRoutes } from './app';
 
-const Routes: React.FC = () => {
+import { NavigationContainer } from '@react-navigation/native';
+
+import { AuthRoutes } from './auth.routes';
+import { AppRoutes } from './app.routes';
+
+import { useAuth } from '../hooks/auth';
+
+export function Routes() {
+  const { user } = useAuth();
   return (
     <NavigationContainer>
-      <AppRoutes />
+      {user.id ? <AppRoutes /> : <AuthRoutes />}
     </NavigationContainer>
   );
-};
-
-export default Routes;
+}
