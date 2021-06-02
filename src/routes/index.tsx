d4ccde1c469cbd75bcc98ce55d11117acc93d9a6
Natanael Lima <taelima1997@gmail.com>
2021-06-01 23:33:29 -0300

    fix user object

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,7 +11,7 @@ export function Routes() {
   const { user } = useAuth();
   return (
     <NavigationContainer>
-      {user.id ? <AppRoutes /> : <AuthRoutes />}
+      {user?.id ? <AppRoutes /> : <AuthRoutes />}
     </NavigationContainer>
   );
 }
