0587182ce99a7e1723f35de41f088c80d5aa7702
Natanael Lima <taelima1997@gmail.com>
2021-06-30 00:12:04 -0300

    update RenderWithTheme to RenderWithProviders

--- src/__tests__/RenderWithProviders.tsx
+++ src/__tests__/RenderWithProviders.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components/native';
 import theme from '../global/styles/theme';
 
-const RenderWithTheme: React.FC = ({ children }) => {
+const RenderWithProviders: React.FC = ({ children }) => {
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 };
 
-export default RenderWithTheme;
+export default RenderWithProviders;
--- src/__tests__/components/Forms/Input.spec.tsx
+++ src/__tests__/components/Forms/Input.spec.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 import { render } from '@testing-library/react-native';
 import { Input } from '../../../components/Forms/Input';
-import RenderWithTheme from '../../RenderWithTheme';
 import theme from '../../../global/styles/theme';
+import RenderWithProviders from '../../RenderWithProviders';
 
 describe('Input componet', () => {
   it('must have specific border color when active', () => {
     const { getByTestId } = render(<Input testID="input" active />, {
-      wrapper: RenderWithTheme,
+      wrapper: RenderWithProviders,
     });
 
     expect(getByTestId('input').props.style[0].borderColor).toEqual(
