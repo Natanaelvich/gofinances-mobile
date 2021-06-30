985c60bb4e3e802e4a98115d184e0becbf1bb976
Natanael Lima <taelima1997@gmail.com>
2021-06-29 23:13:24 -0300

    add setupFilesAfterEnv

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
     'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)',
   ],
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  testPathIgnorePatterns: ['/node_modules', '/android', '/ios'],
   collectCoverage: true,
   collectCoverageFrom: [
     'src/screens/**/*.tsx',
