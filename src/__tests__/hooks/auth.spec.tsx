a304348ff90bec3d86da2f297d6a27153b015c10
Natanael Lima <taelima1997@gmail.com>
2021-06-30 23:47:48 -0300

    add mock expo-google-app-auth and update tests auth hook

--- src/__tests__/hooks/auth.spec.tsx
+++ src/__tests__/hooks/auth.spec.tsx
@@ -1,25 +1,30 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { renderHook, act } from '@testing-library/react-hooks';
 import { AuthProvider, useAuth } from '../../hooks/auth';
 
+jest.mock('expo-google-app-auth', () => {
+  return {
+    logInAsync: () => {
+      return {
+        type: 'success',
+        user: {
+          id: 'user-123',
+          name: 'John doe hahahaah',
+          email: 'johndoe@example.com.br',
+          photoUrl: '',
+        },
+      };
+    },
+  };
+});
+
 describe('Auth hook', () => {
   it('should be able make sing in with google', async () => {
-    const user = {
-      id: 'user-123',
-      name: 'John doe hahahaah',
-      email: 'johndoe@example.com.br',
-    };
-    const token = 'token-123';
-
-    const { result, waitForNextUpdate } = renderHook(() => useAuth(), {
+    const { result } = renderHook(() => useAuth(), {
       wrapper: AuthProvider,
     });
 
     await act(() => result.current.signInWithGoogle());
 
-    // expect(AsyncStorage.setItem).toHaveBeenCalledWith([
-    //   ['@gofinacen:user', token],
-    // ]);
     expect(result.current.user.email).toEqual('johndoe@example.com.br');
   });
 });
