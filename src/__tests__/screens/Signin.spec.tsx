c993913fc0e9ad3437872f6dcfb34b2044af9742
Natanael Lima <taelima1997@gmail.com>
2021-07-01 12:11:02 -0300

    add initial tests Signin screen

--- src/__tests__/screens/Signin.spec.tsx
+++ src/__tests__/screens/Signin.spec.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
+import RenderWithProviders from '../RenderWithProviders';
 import { Signin } from '../../screens/Signin';
 
-test('form submits two answers', () => {
-  const { debug } = render(<Signin />);
+describe('Signin screen', () => {
+  it('shoul be open modal category select', () => {
+    const { debug } = render(<Signin />, {
+      wrapper: RenderWithProviders,
+    });
 
-  debug();
+    debug();
+  });
 });
