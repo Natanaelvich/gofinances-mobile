85fa304a2e88978b20ffc992fe8bc16094ee3e2a
Natanael Lima <taelima1997@gmail.com>
2021-07-03 08:59:30 -0300

    add testMatch to jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'jest-expo',
+  testMatch: ['**/*.spec.ts','**/*.spec.tsx'],
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)',
   ],
